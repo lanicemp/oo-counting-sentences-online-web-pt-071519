3b33baf2a3c2d05ee38390e22232a25e2c16b52b
LaNice Powell <lanicemp@aol.com>
2019-08-05 20:08:56 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -17,11 +17,12 @@ class String
   def count_sentences
     new_sentence = []
     sentence_array = self.split(/[.!?]/)
+    
 #noEmptyCities = cities.reject { |c| c.empty? }
     #sentence_array.reject(&:blank?)
     #return sentence_array
     #if sentence_array.include("").delete 
-    sentence_array.length
+    
   #end 
   end
 end
